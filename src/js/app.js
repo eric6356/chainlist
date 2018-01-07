25cfdfa8841dc1344b0303f04526654e32d16226
Zhognjie He <eric6356@gmail.com>
2018-01-07 13:30:12 -0500

    init frontend

--- src/js/app.js
+++ src/js/app.js
@@ -1,68 +1,25 @@
 App = {
-  web3Provider: null,
-  contracts: {},
+    web3Provider: null,
+    contracts: {},
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    init: function () {
+        // Load article
+        var articlesRow = $('#articlesRow');
+        var articleTemplate = $('#articleTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+        articleTemplate.find('.panel-title').text('article one');
+        articleTemplate.find('.article-description').text('Description for this article');
+        articleTemplate.find('.article-price').text('10.23');
+        articleTemplate.find('.article-seller').text('');
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+        articlesRow.append(articleTemplate.html());
 
-    return App.initWeb3();
-  },
-
-  initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function(adopters, account) {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
+        return App.initWeb3();
+    }
 };
 
-$(function() {
-  $(window).load(function() {
-    App.init();
-  });
+$(function () {
+    $(window).load(function () {
+        App.init();
+    });
 });
