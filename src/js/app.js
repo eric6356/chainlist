b413249d0658d8af082dce1b7d39fc52076b2ac9
Zhognjie He <eric6356@gmail.com>
2018-01-07 22:41:40 -0500

    frontend buy

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,7 @@ App = {
             web3 = new Web3(App.web3Provider);
         }
         App.displayAccountInfo();
-        App.initContract();
+        return App.initContract();
     },
 
     displayAccountInfo: function () {
@@ -47,7 +47,7 @@ App = {
             App.listenToEvents();
 
             // Retrieve the article from the smart contract.
-            App.reloadArticles();
+            return App.reloadArticles();
         })
     },
 
@@ -67,17 +67,32 @@ App = {
             var articleRow = $('#articlesRow');
             articleRow.empty();
 
+            var price = web3.fromWei(article[4], 'ether');
+
             // Retrieve and fill the article template
             var articleTemplate = $('#articleTemplate');
-            articleTemplate.find('.panel-title').text(article[1]);
-            articleTemplate.find('.article-description').text(article[2]);
-            articleTemplate.find('.article-price').text(web3.fromWei(article[3], 'ether'));
+            articleTemplate.find('.panel-title').text(article[2]);
+            articleTemplate.find('.article-description').text(article[3]);
+            articleTemplate.find('.article-price').text(price);
+            articleTemplate.find('.btn-buy').attr('data-value', price);
 
             var seller = article[0];
             if (seller === App.account) {
                 seller = 'You';
             }
             articleTemplate.find('.article-seller').text(seller);
+
+            var buyer = article[1];
+            if (buyer === App.account) {
+                buyer = 'You';
+            } else if (parseInt(buyer) === 0) {
+                buyer = 'No one yet';
+            }
+            articleTemplate.find('.article-buyer').text(buyer);
+
+            if (seller === 'You' || parseInt(article[1]) !== 0x0) {
+                articleTemplate.find('.btn-buy').parent().hide();
+            }
 
             // add this new article
             articleRow.append(articleTemplate.html());
@@ -90,7 +105,7 @@ App = {
         // Retrieve details of th the article
         var _article_name = $('#article_name').val();
         var _description = $('#article_description').val();
-        var _price = web3.toWei(parseInt($('#article_price').val() || 0), 'ether');
+        var _price = web3.toWei(new web3.BigNumber($('#article_price').val() || 0), 'ether');
 
         if ((_article_name.trim() === '') || (_price === 0)) {
             // nothing to sell
@@ -116,9 +131,45 @@ App = {
                 {},
                 {fromBlock: 0, toBlock: 'latest'}
             ).watch(function (error, event) {
-                $('#events').append('<li class="list-group-item">' + event.args._name + ' is for sale</li>');
+                var articleName = event.args._name;
+                var seller = event.args._seller;
+                if (seller === App.account) {
+                    seller = 'You';
+                }
+                var message = articleName + ' is for sale, seller: ' + seller;
+                $('#events').append('<li class="list-group-item">' + message + '</li>');
                 App.reloadArticles();
+            });
+
+            // instance.buyArticleEvent(
+            //     {},
+            //     {fromBlock: 0, toBlock: 'latest'}
+            // ).watch(function (error, event) {
+            //     var articleName = event.args._name;
+            //     var buyer = event.args._buyer;
+            //     if (buyer === App.account) {
+            //         buyer = 'You';
+            //     }
+            //     var message = articleName + ' is sold to ' + buyer;
+            //     $('#events').append('<li class="list-group-item">' + message + '</li>');
+            //     App.reloadArticles();
+            // })
+        })
+    },
+
+    buyArticle: function () {
+        var _price = new web3.BigNumber($(event.target).data('value'));
+
+        App.contracts.ChainList.deployed().then(function (instance) {
+            return instance.buyArticle({
+                from: App.account,
+                value: web3.toWei(_price, 'ether'),
+                gas: 500000
             })
+        }).then(function (result) {
+            console.log(result);
+        }).catch(function (reason) {
+            console.log(reason.message);
         })
     }
 };
