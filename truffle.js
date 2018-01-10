fda3777e612b07efb8e8fdeb03c064dc42a1c67e
Zhognjie He <eric6356@gmail.com>
2018-01-10 12:07:06 -0500

    add rinkeby to truffle

--- truffle.js
+++ truffle.js
@@ -6,6 +6,13 @@ module.exports = {
             host: "127.0.0.1",
             port: 9545,
             network_id: "*" // Match any network id
+        },
+        rinkeby: {
+            host: "127.0.0.1",
+            port: 8545,
+            network_id: "4",
+            gas: 5000000,
+            from: "0x0d6eafe9ca0258b97839b07230a7ea8fa61b632a"
         }
     }
 };
